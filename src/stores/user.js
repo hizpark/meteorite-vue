9ae1e065d5ebaf783ba530620a874c1e537c8f95
Harper Jang <harper.jang@outlook.com>
2025-09-19 18:02:58 +0800

    fix: 分页参数调整

--- src/stores/user.js
+++ src/stores/user.js
@@ -19,7 +19,7 @@ export const useUserStore = defineStore('user', {
       delete: false,
     },
     currentPage: 1,
-    pageSize: 10,
+    pageSize: 15,
     totalCount: 0,
     error: null,
     columns: [
